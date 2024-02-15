48a5078b63e83d8f4341d966e821281bd9d23332
TUD22 <137616846+TUD22@users.noreply.github.com>
2024-02-15 22:21:11 +0100

    gotowe

--- quiz/src/Quiz.jsx
+++ quiz/src/Quiz.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Pytanie from "./Pytanie";
+import './Quiz.css'
 
 function Quiz() {
 
@@ -22,33 +23,51 @@ function Quiz() {
     {
         pytanie: 'd',
         odpowiedz: 'd'
-    }]
+    }
+]
 
+    const [wynik, setWynik]= useState('')
     const [odpowiedz, setOdpowiedz] = useState('')
     const [licznik, setLicznik] = useState(0)
     const [punkty, setPunkty] = useState(0)
     const [pytanie, setPytanie] = useState(pio[licznik])
 
     function nastepnePytanie(){
-        
-        if (pytanie.odpowiedz === odpowiedz.target.value) {
-            setPunkty(punkty+1)
-        }
+        console.log(licznik)
+        setPytanie(pio[licznik])
         if(licznik<4){
+            if (pytanie.odpowiedz == odpowiedz.target.value) {
+                setPunkty(punkty+1)
+            }
             setLicznik(licznik+1)
         }
-        setPytanie(pio[licznik])
-        console.log(punkty)
+        if(licznik==4){
+            document.getElementById('wynik').style.display='block';
+        }
+        
     }
     
+    useEffect(()=>{
+        setPytanie(pio[licznik]);
+        setWynik(punkty)
+    })
 
+    function start(){
+        setLicznik(0)
+        setWynik('')
+        setPunkty(0)
+        document.getElementById('wynik').style.display='none'
+
+    }
   return (
     <div className="Quiz">
-      <button>Rozpocznij grę</button>
-      <button>Zakończ grę</button>
+      <button  className='Button' onClick={start}>Rozpocznij grę</button>
       <Pytanie pytanie={pytanie.pytanie}/>
-      <input onChange={setOdpowiedz}/>
-      <button onClick={nastepnePytanie}>Sprawdź odpowiedź </button>
+      <input onChange={setOdpowiedz}/><br/>
+      <button className='Button' onClick={nastepnePytanie}>Sprawdź odpowiedź </button>
+      <div id='wynik'>
+        Twój winik to: {wynik}
+      </div>
     </div>
   );
 }
