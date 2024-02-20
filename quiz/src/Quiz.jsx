b846b0bee0cf9d0cdfbde5e64042bb2b070f6856
TenCoDzwoni <anrill.aasimar@gmail.com>
2024-02-20 09:33:17 +0100

    Kogo Nienawidzą Giganci? - CZARNYCH

--- quiz/src/Quiz.jsx
+++ quiz/src/Quiz.jsx
@@ -5,24 +5,56 @@ import './Quiz.css'
 function Quiz() {
 
     const pio=[{
-        pytanie: 'ile',
-        odpowiedz: 'tyle'
+        pytanie: 'W którym roku powstała gra Heores of Might and Magic 3?',
+        odpowiedz: '1999'
     },
     {
-        pytanie: 'a',
-        odpowiedz: 'a'
+        pytanie: 'Fabularnie kto jest najpotężniejszym nekromantą kampanii Homm3?',
+        odpowiedz: 'Sandro'
     },
     {
-        pytanie: 'b',
-        odpowiedz: 'b'
+        pytanie: 'Jaki budynek powinno się starać rozbudować w 1 tygodniu gry?',
+        odpowiedz: 'Kapitol'
     },
     {
-        pytanie: 'c',
-        odpowiedz: 'c'
+        pytanie: 'Kogo nienawidzą Giganci?',
+        odpowiedz: 'Czarnych Smoków'
     },
     {
-        pytanie: 'd',
-        odpowiedz: 'd'
+        pytanie: 'Jakie Jednostki produkuje dworek?',
+        odpowiedz: 'Wampiry'
+    },
+    {
+        pytanie: 'Z jakiego typu bronią posługują się krasnoludy?',
+        odpowiedz: 'Młotem'
+    },
+    {
+        pytanie: 'Jak nazywa się zamek w którym można wybudować podziemny tunel?',
+        odpowiedz: 'Twierdza'
+    },
+    {
+        pytanie: 'Ile złota dziennie dostarcza kapitol?',
+        odpowiedz: '4000'
+    },
+    {
+        pytanie: 'Po budowli "Fort" Występuje?',
+        odpowiedz: 'Cytadela'
+    },
+    {
+        pytanie: 'Ilu maksymalnie graczy może grać na jednej mapie?',
+        odpowiedz: '8'
+    },
+    {
+        pytanie: 'Ile kryształów dziennnie dostarcza "Labolatorium"?',
+        odpowiedz: '0'
+    },
+    {
+        pytanie: 'Miasto bagniste z NAJLEPSZĄ muzyką nazywa się?',
+        odpowiedz: 'Cytadela'
+    },
+    {
+        pytanie: 'Ile kosztuje księga czarów?',
+        odpowiedz: '500'
     }
 ]
 
